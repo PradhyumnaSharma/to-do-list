10288093f888300788005349659992e2b980aade
PradhyumnaSharma <spradhyumna2@gmail.com>
2023-05-15 15:23:17 +0530

    hii

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:to_do_app/screens/home_screen.dart';
 
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
@@ -10,15 +11,30 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (param) {
+          return AlertDialog(
+            title: Text('Categories from'),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("categories"),
           leading: ElevatedButton(
-              onPressed: () {},
-              child: Icon(Icons.arrow_back),
-              color = Colors.white),
+            onPressed: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => HomeScreen())),
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+          ),
         ),
         body: Center(child: Text('welcome to categories')),
         floatingActionButton: FloatingActionButton(
